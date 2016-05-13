4bafafdd6481643d89df3c01c04a52246f0ff72d
Cein Markey <hi@ceinmarkey.co.uk>
2016-05-13 15:45:51 +0100

    Update RomanLibrary-spec.js
    Edit tests to accommodate new methods in the Roman library

--- spec/unit/RomanLibrary-spec.js
+++ spec/unit/RomanLibrary-spec.js
@@ -3,29 +3,29 @@ var Roman = require('../../libraries/Roman');
 describe('The `Roman` numeral conversion library', function () {
     describe('when using the `getIntegerToRoman` method', function() {
         it('should return `123` when given `CXXIII`', function() {
-            expect(Roman.getIntegerToRoman('CXXIII')).toBe(123);
+            expect(Roman.getIntegerFromRoman('CXXIII')).toBe(123);
         });
 
         it('should return `3999` when given `MMMCMXCIX`', function() {
-            expect(Roman.getIntegerToRoman('MMMCMXCIX')).toBe(3999);
+            expect(Roman.getIntegerFromRoman('MMMCMXCIX')).toBe(3999);
         });
 
         it('should return `0` when given an empty string', function() {
-            expect(Roman.getIntegerToRoman('')).toBe(0);
+            expect(Roman.getIntegerFromRoman('')).toBe(0);
         });
     });
 
     describe('when using the `getRomanToInteger` method', function() {
         it('should return `CXXIII` when given `123`', function() {
-            expect(Roman.getRomanToInteger(123)).toBe('CXXIII');
+            expect(Roman.getRomanFromInteger(123)).toBe('CXXIII');
         });
 
         it('should return `MMMCMXCIX` when given `3999`', function() {
-            expect(Roman.getRomanToInteger(3999)).toBe('MMMCMXCIX');
+            expect(Roman.getRomanFromInteger(3999)).toBe('MMMCMXCIX');
         });
 
         it('should return an empty string when given `0`', function() {
-            expect(Roman.getRomanToInteger(0)).toBe('');
+            expect(Roman.getRomanFromInteger(0)).toBe('');
         });
     });
 });
